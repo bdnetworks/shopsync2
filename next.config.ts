e2a588014d91f97e26d37c0c1d4395e11a6ef55e
Shakibuzzaman <saakib.com@gmail.com>
2025-11-02 16:05:28 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -51,6 +51,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'www.banglashoppers.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: '6000-firebase-shopsync2-1761566564683.cluster-w5vd22whf5gmav2vgkomwtc4go.cloudworkstations.dev',
         port: '',
         pathname: '/**',
